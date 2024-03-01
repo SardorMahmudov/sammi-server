107927e572acca50b2318d2bb73465eca2811be5
Sardor <ozdemeir09@gmail.com>
2024-03-01 06:02:13 +0500

    Authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CourseModule } from './course/course.module';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 import { getMongoDBConfig } from './config/mongo.config';
+import { CourseModule } from './course/course.module';
 import { UserModule } from './user/user.module';
 
 @Module({
--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,16 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { RoleUser } from './user.interface';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({ timestamps: true })
 export class User {
   @Prop({ unique: true, required: true })
   email: string;
+
   @Prop({ required: true })
   fullName: string;
+
   @Prop({ required: true })
-  passwordHash: string;
+  password: string;
+
+  @Prop()
+  role: RoleUser;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
